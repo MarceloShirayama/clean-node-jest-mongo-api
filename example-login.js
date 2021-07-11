a9c2071f7ae97b5b00ff5946cdca83526eead653
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-07-11 13:23:29 -0300

    refactor validation and create user to SignupUseCase.signup

--- example-login.js
+++ example-login.js
@@ -1,19 +1,30 @@
-const express = require('express')
-const router = express.Router()
-const mongoose = require('mongoose')
-const AccountModel = mongoose.model('Account')
-
+// export routes
 module.exports = () => {
   router.post('/signup', new SignupRouter().route)
 }
 
+// signup-router
+const express = require('express')
+const router = express.Router()
 class SignupRouter {
   async route (req, res) {
     const { email, password, repeatPassword } = req.body
+    const user = new SignupUseCase().signup(email, password, repeatPassword)
+    if (!user) {
+      res.status(400).json({ error: 'password must be equal to repeatPassword' })
+    }
+    res.status(200).json({ message: 'User created successfully'})
+  }
+}
+
+// signup-usecase
+const mongoose = require('mongoose')
+const AccountModel = mongoose.model('Account')
+class SignupUseCase {
+  async signup (email, password, repeatPassword) {
     if (password === repeatPassword) {
       const user = await AccountModel.create({ email, password })
-      return res.json(user)
+      return user
     }
-    res.status(400).json({ error: 'password must be equal to repeatPassword' })
   }
 }
