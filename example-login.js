0b232d91ce730766268e15c9de23810a48e4e6e2
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-07-11 14:25:37 -0300

    refactor SignupUseCase to AddAccountRepository.add

--- example-login.js
+++ example-login.js
@@ -13,18 +13,24 @@ class SignupRouter {
     if (!user) {
       res.status(400).json({ error: 'password must be equal to repeatPassword' })
     }
-    res.status(200).json({ message: 'User created successfully'})
+    res.status(200).json({ message: 'User created successfully' })
   }
 }
 
 // signup-usecase
-const mongoose = require('mongoose')
-const AccountModel = mongoose.model('Account')
 class SignupUseCase {
   async signup (email, password, repeatPassword) {
     if (password === repeatPassword) {
-      const user = await AccountModel.create({ email, password })
-      return user
+      new AddAccountRepository().add(email, password)
     }
+  }
+}
+
+const mongoose = require('mongoose')
+const AccountModel = mongoose.model('Account')
+class AddAccountRepository {
+  async add (email, password) {
+    const user = await AccountModel.create({ email, password })
+    return user
   }
 }
