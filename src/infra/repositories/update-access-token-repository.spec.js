ff2b8d3738a71ad773bf28e5e762840576126d52
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-07-22 21:04:10 -0300

    ensure UpdateAccessTokenRepository throw error if userModel is not provided

--- src/infra/repositories/update-access-token-repository.spec.js
+++ src/infra/repositories/update-access-token-repository.spec.js
@@ -47,4 +47,19 @@ describe('Update Access Token Repository', () => {
     const updatedFakeUser = await userModel.findOne({ _id })
     expect(updatedFakeUser.accessToken).toBe('valid_token')
   })
+
+  it('Should throw error if userModel is not provided', async () => {
+    const sut = new UpdateAccessTokenRepository()
+    const userModel = db.collection('users')
+    const fakeUser = await userModel.insertOne({
+      email: 'valid_email@mail.com',
+      name: 'any_name',
+      age: 50,
+      state: 'any_state',
+      password: 'hashed_password'
+    })
+    const { _id } = fakeUser.ops[0]
+    const promise = sut.update(_id, 'valid_token')
+    expect(promise).rejects.toThrow()
+  })
 })
