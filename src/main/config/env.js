3f70e1a0b99c96ea16ee8114fde54bb50d0370ee
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-07-24 16:40:24 -0300

    move loginRouter composition to its own file

--- src/main/config/env.js
+++ src/main/config/env.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://clean-node-api-user:clean-node-api-pass@localhost:27017/clean-node-api'
+  mongoUrl: process.env.MONGO_URL || 'mongodb://clean-node-api-user:clean-node-api-pass@localhost:27017/clean-node-api',
+  tokenSecret: process.env.TOKEN_SECRET || 'secret_key'
 }
