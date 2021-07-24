c9ed950b4b739c4fb0cea64009938504be161572
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-07-24 09:50:24 -0300

    move headers setup to own file

--- src/main/config/setup.js
+++ src/main/config/setup.js
@@ -1,9 +1,6 @@
+const headers = require('../middlewares/headers')
+
 module.exports = app => {
   app.disable('x-powered-by')
-  app.use((req, res, next) => {
-    res.set('access-control-allow-origin', '*')
-    res.set('access-control-allow-methods', '*')
-    res.set('access-control-allow-headers', '*')
-    next()
-  })
+  app.use(headers)
 }
