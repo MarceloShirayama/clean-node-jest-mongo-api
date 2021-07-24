0d0d992e5966a4c45041ff824feac622c12d8387
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-07-24 09:26:19 -0300

    ensure X-Powered-By: Express returns undefined if x-powered-by is disabled

---
+++ src/main/config/setup.test.js
@@ -0,0 +1,13 @@
+const request = require('supertest')
+const app = require('./app')
+
+describe('App Setup', () => {
+  it('Should X-Powered-By: Express returns undefined if x-powered-by is disabled ', async () => {
+    app.get('/test_x_powered_by', (req, res) => {
+      res.send('')
+    })
+    const response = await request(app).get('/test_x_powered_by')
+    console.log(response.headers)
+    expect(response.headers['x-powered-by']).toBeUndefined()
+  })
+})
